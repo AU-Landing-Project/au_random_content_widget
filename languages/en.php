093c6cfbbdda14c663b75c4bbafde9cbd4975486
Matt Beckett <beck24@gmail.com>
2012-09-17 10:26:31 -0600

    Adds widget description

--- languages/en.php
+++ languages/en.php
@@ -11,6 +11,7 @@ $english = array(
   'au_random_content:no:subtypes' => 'No content types have been selected',
   'au_random_content:no:results' => 'No content to display',
   'au_random_content:mintime' => "How far back to look for content?<br>Enter a date YYYY-MM-DD, or a relative amount of time eg. '-6 weeks'",
+  'au_random_content:widget:description' => 'Display a random sample of content from throughout the site',
 );
 
 
